4040a9e43bec4ecdc189cca24d2bac5631b160a5
jong-k <fully.charged07@gmail.com>
2023-10-31 21:39:00 +0900

    project init && server setup

---
+++ server/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
+import twilio from "twilio";
+
+const PORT = process.env.PORT || 5002;
+import { Server } from "socket.io";
+import { router } from "./router";
+// localhost 3000 에서만 접속 가능
+const ALLOWED_ORIGINS = ["http://localhost:3000"];
+
+const app = express();
+app.use(router);
+app.use(cors());
+
+const expressServer = app.listen(PORT, () =>
+  console.log(`Server is listening on ${PORT}`),
+);
+const io = new Server(expressServer, {
+  cors: {
+    origin: ALLOWED_ORIGINS,
+    methods: ["GET", "POST"],
+  },
+});
