e3a5edc1ace547c2668833c627569d9f3902c99a
jong-k <fully.charged07@gmail.com>
2023-11-04 13:04:13 +0900

    feat: host params 확인기능 추가

--- client/src/pages/JoinRoomPage/index.tsx
+++ client/src/pages/JoinRoomPage/index.tsx
@@ -1,5 +1,18 @@
-// import s from "./index.module.scss";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import s from "./index.module.scss";
 
 export default function JoinRoomPage() {
-  return <div>Hello from JoinRoomPage</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const isRoomHost = searchParams.get("host");
+    if (isRoomHost) console.log("host!!");
+  }, []);
+
+  return (
+    <div className={s.joinRoomPageContainer}>
+      <div className={s.joinRoomPagePanel}></div>
+    </div>
+  );
 }
