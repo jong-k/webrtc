7bf443ccde27569457826a0c4d1666011bb33e0f
jong-k <fully.charged07@gmail.com>
2023-11-09 19:12:05 +0900

    feat: RTC Data Channel page 완성

--- client/src/pages/TestExchangeDataPage/index.tsx
+++ client/src/pages/TestExchangeDataPage/index.tsx
@@ -4,8 +4,20 @@ export default function TestExchangeDataPage() {
   return (
     <div>
       <h2>RTC Data Channel Test</h2>
-      <div>
-        <button type="button">시작</button>
+      <div className={s.textareaBox}>
+        <textarea className={s.textarea} cols={40} rows={10}></textarea>
+        <textarea className={s.textarea} cols={40} rows={10}></textarea>
+      </div>
+      <div className={s.buttonBox}>
+        <button className={s.button} type="button">
+          시작
+        </button>
+        <button className={s.button} type="button">
+          ✉️
+        </button>
+        <button className={s.button} type="button">
+          ❌
+        </button>
       </div>
     </div>
   );
