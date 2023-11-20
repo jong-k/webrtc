240778dcdd1211313f64d9b8c21f8d89b89e5a89
jong-k <fully.charged07@gmail.com>
2023-11-20 12:19:13 +0900

    feat: test server 설정

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,10 @@ import cors from "cors";
 // import { v4 as uuidv4 } from "uuid";
 import { Server } from "socket.io";
 import { router } from "./router";
-import { Message } from "./types";
 
-// localhost 5173 에서만 접속 가능
-const ALLOWED_ORIGINS = ["http://localhost:5001"];
-const PORT = process.env.PORT || 5002;
+// localhost 8001 에서만 접속 가능
+const ALLOWED_ORIGINS = ["http://localhost:8001"];
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(router);
@@ -16,6 +15,7 @@ app.use(cors());
 const expressServer = app.listen(PORT, () =>
   console.log(`Server is listening on ${PORT}`),
 );
+
 const io = new Server(expressServer, {
   cors: {
     origin: ALLOWED_ORIGINS,
@@ -23,14 +23,12 @@ const io = new Server(expressServer, {
 });
 
 io.on("connection", (socket) => {
-  // logger
-  const logger = (text: string) => {
-    const message = `Message from server: ${text}`;
-    socket.emit("logger", message);
-  };
-
-  socket.on("message", (message: string) => {
-    logger(`Client said: ${message}`);
+  socket.on("message", (message: any) => {
+    console.log("message from client", message);
     socket.broadcast.emit("message", message);
+  });
+
+  socket.on("bye", () => {
+    console.log("received bye");
   });
 });
